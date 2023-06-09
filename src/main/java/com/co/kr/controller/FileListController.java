bef0728b859a60edb09eca188e5b1d02917c72d9
SBK <moran805@git.email>
2023-06-09 20:40:32 +0900

    first commit

---
+++ src/main/java/com/co/kr/controller/FileListController.java
@@ -0,0 +1,42 @@
+package com.co.kr.controller;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.co.kr.service.UploadService;
+import com.co.kr.vo.FileListVO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class FileListController {
+	
+	@Autowired
+	private UploadService uploadService;
+
+	
+	@PostMapping(value = "upload")
+	public ModelAndView bdUpload(FileListVO fileListVO, MultipartHttpServletRequest request, HttpServletRequest httpReq) throws IOException, ParseException {
+		
+		ModelAndView mav = new ModelAndView();
+		int bdSeq = uploadService.fileProcess(fileListVO, request, httpReq);
+		fileListVO.setContent(""); //초기화
+		fileListVO.setTitle(""); //초기화
+		
+		// 화면에서 넘어올때는 bdSeq String이라 string으로 변환해서 넣어즘
+		// mav = bdSelectOneCall(fileListVO, String.valueOf(bdSeq),request);
+		mav.setViewName("board/boardList.html");
+		return mav;
+		
+	}
+
+}
